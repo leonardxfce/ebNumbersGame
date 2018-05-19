1004025c543b62dd6cb1c6cf0fb97d51ae1c7d31
Leonardxfce <leonardo.araoz.dev@gmail.com>
2018-05-19 19:22:20 -0300

    agrego el manejador de consola

---
+++ src/HumanoNumero.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace EB;
+
+class HumanoNumero  implements Jugable{
+    public function __construct() {
+        ;
+    }
+    public function analizar($datoDelOtroJugador) {
+        
+    }
+
+    public function decir() {
+        
+    }
+
+    public function pensar() {
+        
+    }
+
+}
---
+++ src/UtilConsola.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace EB;
+
+class UtilConsola {
+
+    public static function crearManejadorConsola() {
+        return new UtilConsola();
+    }
+
+    public function leer() {
+        return strtoupper(trim(fgets(STDIN)));
+    }
+
+    public function mostrar($msg) {
+        fwrite(STDOUT, "$msg \n");
+    }
+
+}
