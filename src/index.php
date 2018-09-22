08eddd866b9628d839b4739a725c93b2f0393e1c
leonardxfce <leonardo.araoz.dev@gmail.com>
2018-09-22 12:20:39 -0300

    trying make works this

--- src/index.php
+++ src/index.php
@@ -1,15 +1,19 @@
 <?php
 
 namespace EB;
+require 'vendor/autoload.php';
 
-require '../vendor/autoload.php';
-
-$consola = UtilConsola::crearManejadorConsola();
-$partida = Partida::crear($consola);
-do {
-    $partida->configurar();
-    $partida->jugar();
-    $consola->mostrar("Seguir Jugando");
-    $opcion = $consola->leer();
-    $foo = $partida->seguirJugando($opcion);
-} while ($foo);
+class Inicio{
+    public function Iniciar()
+    {
+        $consola = UtilConsola::crearManejadorConsola();
+        $partida = Partida::crear($consola);
+        do {
+            $partida->configurar();
+            $partida->jugar();
+            $consola->mostrar("Seguir Jugando");
+            $opcion = $consola->leer();
+            $foo = $partida->seguirJugando($opcion);
+        } while ($foo);
+    }
+}
